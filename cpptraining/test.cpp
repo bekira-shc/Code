0e09eb915936457c69117529522f4a56f1dbd4a4
bekira-shc <musashinoshc@gmail.com>
2020-12-01 00:40:58 +0900

    20201201

--- cpptraining/test.cpp
+++ cpptraining/test.cpp
@@ -1,52 +1,9 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <io.h>
-#include <fstream>
-
- void getFiles( std::string path, std::vector<std::string>& files )  
- {  
-
-    long   hFile   =   0;    
-    struct _finddata_t fileinfo;  
-    std::string p;  
-    if((hFile = _findfirst(p.assign(path).append("\\*").c_str(),&fileinfo)) !=  -1)  
-    {  
-        do  
-        {   
-            if((fileinfo.attrib &  _A_SUBDIR))  
-            {  
-                if(strcmp(fileinfo.name,".") != 0  &&  strcmp(fileinfo.name,"..") != 0)
-                files.push_back(fileinfo.name);
-                //getFiles( p.assign(path).append("\\").append(fileinfo.name), files );  
-            }  
-            else  
-            {  
-                continue; 
-            }  
-        }
-        while(_findnext(hFile, &fileinfo)  == 0);  
-        _findclose(hFile);  
-     }  
-} 
-
-
-void owf()
-{
-    std::vector<std::string> tem;
-    std::string path = "D:\\Code\\Code\\pytraining";
-
-    getFiles(path,tem);
-
-    for(int i = 0; i < tem.size(); i++)
-    {
-        std::cout << tem[i] << std::endl;
-    }
-}
 
 int main ()
 {
-    owf();
+    std::cout<< "hello world" << std::endl;
 
     return 0;
 }
